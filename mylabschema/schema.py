f0c25dc027c7ca0606f2fc203ba713f63d1d1f9d
Lauri Himanen <lauri.himanen@gmail.com>
2023-04-14 11:53:03 +0300

    Updated schema.

--- mylabschema/schema.py
+++ mylabschema/schema.py
@@ -1,49 +1,32 @@
-import datetime
 import uuid
+import datetime
+from typing import List
+
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.signal import find_peaks, savgol_filter
+
 from nomad.units import ureg
-from nomad.metainfo import Quantity, Package, Section
-from nomad.datamodel.results import (
-    Results,
-    Material,
-    Properties,
-    ElectronicProperties,
-    BandStructureElectronic,
-    BandGap
-)
+from nomad.metainfo import Quantity, Package
 from nomad.datamodel.metainfo.eln.material_library import Sample
-from nomad.datamodel.metainfo.annotations import (
-    ELNAnnotation,
-    ELNComponentEnum,
-    BrowserAnnotation,
-    BrowserAdaptors
-)
+from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
 
 m_package = Package()
 
 
 class MySample(Sample):
-    m_def = Section(
-        label='MySample',
-        properties={
-            'editable': {
-                'exclude': ['institute']
-            }
-        }
-    )
-    composition_file = Quantity(
+    x_ray_fluorescence_file = Quantity(
         type=str,
-        label='Composition file',
-        a_eln=ELNAnnotation(component=ELNComponentEnum.FileEditQuantity),
-        a_browser=BrowserAnnotation(adaptor=BrowserAdaptors.RawFileAdaptor)
+        a_eln=ELNAnnotation(
+            label='X-ray fluorescence file',
+            component=ELNComponentEnum.FileEditQuantity
+        )
     )
     uv_vis_spectrum_file = Quantity(
         type=str,
-        label='UV-vis spectrum file',
-        a_eln=ELNAnnotation(component=ELNComponentEnum.FileEditQuantity),
-        a_browser=BrowserAnnotation(adaptor=BrowserAdaptors.RawFileAdaptor)
+        a_eln=ELNAnnotation(
+            label='UV-vis spectrum file',
+            component=ELNComponentEnum.FileEditQuantity
+        )
     )
 
     def normalize(self, archive, logger):
@@ -57,38 +40,23 @@ class MySample(Sample):
         if not self.sample_id:
             self.sample_id = str(uuid.uuid4())
 
-        # Read in the composition information and record it in the processed data
-        if self.composition_file:
-            with archive.m_context.raw_file(self.composition_file) as f:
+        # Read in the X-ray fluorescence file, extract composition and save in common metainfo
+        if self.x_ray_fluorescence_file:
+            with archive.m_context.raw_file(self.x_ray_fluorescence_file) as f:
                 data = np.loadtxt(f, delimiter=',', dtype=str)
-                elements = data[:, 0]
-                if not archive.results:
-                    archive.results = Results()
-                if not archive.results.material:
-                    archive.results.material = Material()
-                archive.results.material.elements = elements
+                material = m_add(archive, "results.material")
+                material.elements = self.extract_elements(data)
 
-        # Read in the band gap measurement and record it in the processed data
+        # Read in the the UV-Vis measurement, extract band gap and save in common metainfo
         if self.uv_vis_spectrum_file:
             with archive.m_context.raw_file(self.uv_vis_spectrum_file) as f:
                 data = np.loadtxt(f, delimiter=',')
-                band_gap = self.extract_band_gap(data)
-                if not archive.results:
-                    archive.results = Results()
-                if not archive.results.properties:
-                    archive.results.properties = Properties()
-                if not archive.results.properties.electronic:
-                    archive.results.properties.electronic = ElectronicProperties()
-                if not archive.results.properties.electronic.band_structure_electronic:
-                    archive.results.properties.electronic.m_add_sub_section(ElectronicProperties.band_structure_electronic, BandStructureElectronic())
-                if not archive.results.properties.electronic.band_structure_electronic[0].band_gap:
-                    archive.results.properties.electronic.band_structure_electronic[0].m_add_sub_section(BandStructureElectronic.band_gap, BandGap())
-                archive.results.properties.electronic.band_structure_electronic[0].band_gap[0].value = band_gap
-
+                band_gap_section = m_add(archive, "results.properties.electronic.band_structure_electronic.band_gap")
+                band_gap_section.value = self.extract_band_gap(data)
+                archive.results.properties.available_properties = ['electronic.band_structure_electronic.band_gap']
 
     def extract_band_gap(self, data: np.ndarray) -> ureg.Quantity:
         '''Extracts a band gap value from a UV-vis spectra.'''
-        # Extract wavelength and absorbance data
         wavelength = data[:, 0]
         absorbance = data[:, 1]
 
@@ -96,14 +64,47 @@ class MySample(Sample):
         absorbance_smooth = savgol_filter(absorbance, 25, 3)
         absorbance_gradient = -np.gradient(absorbance_smooth)
 
-        # Find peak
+        # Find peak in gradient
         peaks, _ = find_peaks(absorbance_gradient, height=0.015)
         assert len(peaks) == 1
         peak_wavelength = wavelength[peaks[0]]
 
-        # Calculate band gap energy from peak position using eV = hc/lambda
+        # Calculate band gap energy from peak position using eV = h * c / lambda
         band_gap_energy = (ureg.speed_of_light * ureg.planck_constant) / (peak_wavelength * ureg('nanometer'))
         return band_gap_energy.to(ureg.eV)
+
+    def extract_elements(self, data: np.ndarray) -> List[str]:
+        '''Extracts a list of chemical elements from the X-ray fluorescence file.'''
+        return data[:, 0]
+    
+
+def m_add(root, path):
+    parts = path.split(".")
+    for part in parts:
+        child = getattr(root, part)
+        props = root.m_def.all_properties
+        child_section = props[part]
+        child_cls = child_section.sub_section.section_cls
+        repeats = child_section.repeats
+
+        # See if child exists. Repeating subsection is accepted only if there is
+        # one instance.
+        if child:
+            root = child
+            if repeats:
+                if len(child) != 1:
+                    raise ValueError('Cannot add value as path contains a repeating section with multiple entries.')
+                root = child[0]
+        # Otherwise create child
+        else:
+            child_instance = child_cls()
+            if repeats:
+                root.m_add_sub_section(child_section, child_instance)
+            else:
+                setattr(root, part, child_instance)
+            root = child_instance
+
+    return root
 
 
 m_package.__init_metainfo__()
--- tests/create_spectra.py
+++ tests/create_spectra.py
@@ -3,53 +3,90 @@ import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from scipy.signal import savgol_filter
 from nomad.units import ureg
-
-def logistic(x, mu, sig):
-    return 1 / (np.exp((x-mu)/sig) + 1) + 0.02*np.random.random(x.size)
-
-composition = np.loadtxt('tests/data/x_ray_fluorescence.csv', delimiter=',', dtype=str)
-print(composition)
-
-n_points = 500
-x = np.linspace(450, 550, n_points)
-y = logistic(x, 500, 1)
-gradient = np.gradient(y)
-
-data = np.zeros((n_points, 2))
-data[:, 0] = x
-data[:, 1] = y
+from nomad.datamodel import EntryArchive
+from nomad.datamodel.results import Results
 
 
-def extract_band_gap(data: np.ndarray) -> ureg.Quantity:
-    '''Extracts a band gap value from a UV-vis spectra.'''
-    # Extract wavelength and absorbance data
-    wavelength = data[:, 0]
-    absorbance = data[:, 1]
 
-    # Smoothen data, calculate gradient
-    absorbance_smooth = savgol_filter(absorbance, 25, 3)
-    absorbance_gradient = -np.gradient(absorbance_smooth)
+def m_add(root, path, value):
+    parts = path.split(".")
+    for part in parts[:-1]:
+        child = getattr(root, part)
+        props = root.m_def.all_properties
+        child_section = props[part]
+        child_cls = child_section.sub_section.section_cls
+        repeats = child_section.repeats
 
-    # Plot
-    fig, ax = plt.subplots(figsize=(9, 6), layout='constrained')
-    ax.plot(wavelength, absorbance)
-    ax.plot(wavelength, absorbance_smooth)
-    ax.plot(wavelength, absorbance_gradient)
-    ax.set_xlabel("Wavelength (nm)", fontsize=16)
-    ax.set_ylabel("Absorbance (AU)", fontsize=16)
-    plt.show()
+        # See if child exists. Non-repeating sections are accepted as they are,
+        # repeating sections are accepted if there is only one instance
+        if child:
+            root = child
+            if repeats:
+                if len(child) != 1:
+                    raise ValueError('Cannot add value as path contains a repeating section with multiple entries.')
+                root = child[0]
+        # Otherwise create child
+        else:
+            child_instance = child_cls()
+            if repeats:
+                root.m_add_sub_section(child_section, child_instance)
+            else:
+                setattr(root, part, child_instance)
+            root = child_instance
 
-    # Find peak
-    peaks, _ = find_peaks(absorbance_gradient, height=0.015)
-    assert len(peaks) == 1
-    peak_wavelength = wavelength[peaks[0]]
-
-    # Calculate band gap energy from peak position using eV = hc/lambda
-    band_gap_energy = (ureg.speed_of_light * ureg.planck_constant) / (peak_wavelength * ureg('nanometer'))
-    return band_gap_energy.to(ureg.eV)
-
-print(extract_band_gap(data))
+    setattr(root, parts[-1], value)
 
 
-# np.savetxt("uv_vis_spectra.csv", data, delimiter=",")
+a = EntryArchive()
+m_add(a, "results.properties.available_properties", ["test"])
+print(a.results.properties.available_properties)
 
+# def logistic(x, mu, sig):
+#     return 1 / (np.exp((x-mu)/sig) + 1) + 0.02*np.random.random(x.size)
+
+# composition = np.loadtxt('tests/data/x_ray_fluorescence.csv', delimiter=',', dtype=str)
+# print(composition)
+
+# n_points = 500
+# x = np.linspace(470, 530, n_points)
+# y = logistic(x, 500, 1)
+# gradient = np.gradient(y)
+
+# data = np.zeros((n_points, 2))
+# data[:, 0] = x
+# data[:, 1] = y
+
+
+# def extract_band_gap(data: np.ndarray) -> ureg.Quantity:
+#     '''Extracts a band gap value from a UV-vis spectra.'''
+#     # Extract wavelength and absorbance data
+#     wavelength = data[:, 0]
+#     absorbance = data[:, 1]
+
+#     # Smoothen data, calculate gradient
+#     absorbance_smooth = savgol_filter(absorbance, 25, 3)
+#     absorbance_gradient = -np.gradient(absorbance_smooth)
+
+#     # Plot
+#     fig, ax = plt.subplots(figsize=(6, 6), layout='constrained')
+#     ax.plot(wavelength, absorbance, color="orange", linestyle="-", linewidth=2, label="raw data")
+#     ax.plot(wavelength, absorbance_smooth, color="k", linestyle="--", linewidth=1, label="smoothed data")
+#     ax.plot(wavelength, absorbance_gradient, color="blue", linestyle="-", linewidth=2, label="gradient")
+#     ax.set_xlabel("Wavelength (nm)", fontsize=16)
+#     ax.set_ylabel("Absorbance (AU)", fontsize=16)
+#     plt.legend(loc="upper right")
+#     plt.show()
+
+#     # Find peak
+#     peaks, _ = find_peaks(absorbance_gradient, height=0.015)
+#     assert len(peaks) == 1
+#     peak_wavelength = wavelength[peaks[0]]
+
+#     # Calculate band gap energy from peak position using eV = hc/lambda
+#     band_gap_energy = (ureg.speed_of_light * ureg.planck_constant) / (peak_wavelength * ureg('nanometer'))
+#     return band_gap_energy.to(ureg.eV)
+
+# print(extract_band_gap(data))
+
+
+# # np.savetxt("uv_vis_spectra.csv", data, delimiter=",")
