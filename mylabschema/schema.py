851dcb9b3682b1b1406f8a441b8ef92ec31977d3
Lauri Himanen <lauri.himanen@gmail.com>
2023-04-26 10:12:18 +0300

    Updated schema.

--- mylabschema/schema.py
+++ mylabschema/schema.py
@@ -30,6 +30,7 @@ class MySample(Sample):
     )
 
     def normalize(self, archive, logger):
+        # Call the normalize-function of Sample
         super().normalize(archive, logger)
 
         # Prefill institute, datetime, sample_id
@@ -40,14 +41,16 @@ class MySample(Sample):
         if not self.sample_id:
             self.sample_id = str(uuid.uuid4())
 
-        # Read in the X-ray fluorescence file, extract composition and save in common metainfo
+        # Read in the X-ray fluorescence file, extract composition and save in
+        # common metainfo
         if self.x_ray_fluorescence_file:
             with archive.m_context.raw_file(self.x_ray_fluorescence_file) as f:
                 data = np.loadtxt(f, delimiter=',', dtype=str)
-                material = m_get_section(archive, "results.material")
+                material = archive.m_setdefault("results.material")
                 material.elements = data[:, 0]
 
-        # Read in the the UV-Vis measurement, extract band gap and save in common metainfo
+        # Read in the the UV-Vis measurement, extract band gap and save in
+        # common metainfo
         if self.uv_vis_spectrum_file:
             with archive.m_context.raw_file(self.uv_vis_spectrum_file) as f:
                 data = np.loadtxt(f, delimiter=',')
@@ -66,41 +69,8 @@ class MySample(Sample):
                 # Calculate band gap energy from peak position using eV = h * c / lambda
                 band_gap_energy = (ureg.speed_of_light * ureg.planck_constant) / (peak_wavelength * ureg('nanometer'))
 
-                band_gap_section = m_get_section(archive, "results.properties.electronic.band_structure_electronic.band_gap")
-                band_gap_section.value = band_gap_energy
-    
-
-def m_get_section(root, path):
-    '''Given a root section and a path, looks if a unique section can be found
-    under that path and returns it. Will create the sections along the path if
-    no instances are found.
-    '''
-    parts = path.split(".")
-    for part in parts:
-        child = getattr(root, part)
-        props = root.m_def.all_properties
-        child_section = props[part]
-        child_cls = child_section.sub_section.section_cls
-        repeats = child_section.repeats
-
-        # See if child exists. Repeating subsection is accepted only if there is
-        # one instance.
-        if child:
-            root = child
-            if repeats:
-                if len(child) != 1:
-                    raise ValueError('Cannot add value as path contains a repeating section with multiple entries.')
-                root = child[0]
-        # Otherwise create child
-        else:
-            child_instance = child_cls()
-            if repeats:
-                root.m_add_sub_section(child_section, child_instance)
-            else:
-                setattr(root, part, child_instance)
-            root = child_instance
-
-    return root
+                band_gap = archive.m_setdefault("results.properties.electronic.band_structure_electronic.band_gap")
+                band_gap.value = band_gap_energy
 
 
 m_package.__init_metainfo__()
